bc801363a7a5b9e95a38276c59992940f6410572
ChongLeangUENG <leangcnrp777@gmail.com>
2023-01-30 16:30:31 +0100

    Modifie the views

--- website/views.py
+++ website/views.py
@@ -6,3 +6,7 @@ views = Blueprint('views', __name__)
 @views.route('/')
 def home():
     return render_template("index.html")
+
+@views.route('/login.html')
+def login():
+    return render_template("login.html")
