18e628ab3564ae14aa69e3c678837b074ffddadc
Yeh-Hsin LEE <oglanshi@gmail.com>
2023-02-21 16:11:25 +0100

    update for frontend

--- back_font_test/pebblesite/pebblesite/settings.py
+++ back_font_test/pebblesite/pebblesite/settings.py
@@ -38,9 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    #'pebble.apps.PebbleConfig',
-    #'register.apps.RegisterConfig',
-    'account'
+    'account',
 ]
 
 #AUTH_USER_MODEL = "register.CustomUser"
@@ -127,8 +125,13 @@ USE_TZ = True
 
 SITE_NAME = 'pebblesite'
 
-STATIC_URL = '/static/'
-STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+STATIC_URL = 'static/'
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, "static/"),
+]
+
+
 
 #CRISPY_TEMPLATE_PACK='bootstrap4'
 
