a0a7ac8c03e1e744c85bc19b99ee4bc33eb26859
ChongLeangUENG <leangcnrp777@gmail.com>
2023-02-28 16:08:43 +0100

    Clean up code in account

--- pebblesite/account/admin.py
+++ pebblesite/account/admin.py
@@ -19,35 +19,3 @@ class QuestionAdmin(admin.ModelAdmin):
     inlines = [ChoiceInline]
 
 admin.site.register(Question, QuestionAdmin)
-
-'''
-class MatchAdmin(admin.ModelAdmin):
-    list_display = ('user1', 'user2', 'percentage')
-
-admin.site.register(Match, MatchAdmin)
-
-
-class UserAdmin(BaseUserAdmin):
-    list_display = ('username', 'email')
-
-    def get_match_results(self, obj):
-        matches = Match.objects.filter(user1=obj).order_by('-percentage')
-        return [f"{match.user2.username} ({match.percentage}%)"
-                for match in matches]
-    
-    get_match_results.short_description = 'Match Results'
-
-    def get_queryset(self, request):
-        queryset = super().get_queryset(request)
-        queryset = queryset.annotate(match_count=account.models.Count('matches'))
-        return queryset
-
-    def match_count(self, obj):
-        return obj.match_count
-    
-    match_count.short_description = 'Match Count'
-
-    list_display += ('match_count', 'get_match_results')
-
-admin.site.register(User, UserAdmin)
-'''
