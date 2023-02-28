b19436831bf6b296118d366f1713754975bcc565
ChongLeangUENG <leangcnrp777@gmail.com>
2023-02-28 16:11:49 +0100

    Clean up code in account

--- pebblesite/account/admin.py
+++ pebblesite/account/admin.py
@@ -1,10 +1,6 @@
 from django.contrib import admin
 from .models import User, Question, Choice, UserAnswer, Match
 
-# Register your models here.
-
-#User = get_user_model()
-#admin.site.unregister(User)
 
 class ChoiceInline(admin.TabularInline):
     model = Choice
