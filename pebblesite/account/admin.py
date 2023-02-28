3ce1b611880babb2143522d919e83c53d74a6834
ChongLeangUENG <leangcnrp777@gmail.com>
2023-02-28 16:11:34 +0100

    Clean up code in account

--- pebblesite/account/admin.py
+++ pebblesite/account/admin.py
@@ -1,6 +1,4 @@
 from django.contrib import admin
-#from django.contrib.auth.admin import UserAdmin as BaseUserAdmin
-#from django.contrib.auth import get_user_model
 from .models import User, Question, Choice, UserAnswer, Match
 
 # Register your models here.
